7fd546c8e05563438d7947078020562e89fb829a
bpole <bpole@student.21-school.ru>
2019-10-10 13:54:25 +0300

    chened main

--- main.c
+++ main.c
@@ -17,46 +17,93 @@ void    print_tet(t_t *tet)
 {
     while (tet->letter)
     {
-        printf("%c %d %d\n", tet->letter, tet->width, tet->height);
-        print_fig(tet->value);
+        printf("%c %d %d %d\n", tet->letter, tet->x, tet->y, tet->flag);
+        //print_fig(tet->value);
         tet++;
     }
 }
 
-int     solver(t_t *tet)
+int     solver(t_t *tet, unsigned long map, int size, int x, int y)
 {
-    unsigned long   map;
-    int             size;
-    int             x;
-    int             y;
+    t_t             *cpy_tet;
+    unsigned long   tmp;
 
-    x = 0;
-    y = 0;
-    map = 0UL;
-    size = 16;
+    cpy_tet = tet;
+    while (cpy_tet->letter)
+    {
+        if (!cpy_tet->flag)
+        {
+            if (x + cpy_tet->width > size)
+            {
+                x = 0;
+                y++;
+                map >>= 16;
+            }
+            if (y + cpy_tet->height > size)
+                return (0);
+            if (!(map & cpy_tet->value << x))
+            {
+                tmp = map;
+                map |= cpy_tet->value << x;
+                cpy_tet->flag = 1;
+                cpy_tet->x = x;
+                cpy_tet->y = y;
+                print_fig(map);
+                if (solver(tet, map, size, x, y))
+                    return (1);
+                map = tmp;
+                cpy_tet->flag = 0;
+                cpy_tet->x = 0;
+                cpy_tet->y = 0;
+                cpy_tet++;
+                continue;
+            }
+            x++;
+        }
+        else
+            cpy_tet++;
+    }
+    return (1);
+}
+
+void    print_result(t_t *tet, int size)
+{
+    unsigned long   mask;
+    char    *str;
+    int     i;
+
+    mask = 1UL;
+    str = ft_strnew(size * size);
+    ft_memset(str, '.', size * size);
     while (tet->letter)
     {
-        if (x == size)
+        i = 0;
+        while (i < 64)
         {
-            x = 0;
-            y++;
-            map >>= 16;
+            if ((mask << i) & tet->value)
+                str[i % 16  + i / 16 * (size) + tet->x + tet->y * size] = tet->letter;
+            i++;
         }
-        if (!(map & tet->value << x))
-        {
-            map |= tet->value << x;
-            tet++;
-        }
-        print_fig(map);
-        x++;
+        tet++;
     }
-    return (map);
+    i = 0;
+    while (*str && ++i)
+    {
+        ft_putchar(*str++);
+        if (i % size == 0)
+            ft_putchar('\n');
+    }
+    ft_putchar('\n');
 }
 
 int     main(void)
 {
+    unsigned long   map;
+    int             size;
     t_t		*tet;
     int		fd;
+    int             x;
+    int             y;
 
     if (2 != 2)
         return (ft_put_error("usage : ./fillit input_tetraminos"));
@@ -67,8 +114,14 @@ int     main(void)
     ft_bzero(tet, sizeof(t_t) * FIG_MAX + 1);
     if (ft_read_inputfile(tet, fd))
         return (ft_put_error("error"));
-    print_tet(tet);
-    solver(tet);
 
+    x = 0;
+    y = 0;
+    map = 0UL;
+    size = 7;
+    solver(tet, map, size, x, y);
+    print_tet(tet);
+
+    print_result(tet, size);
     return (0);
 }
